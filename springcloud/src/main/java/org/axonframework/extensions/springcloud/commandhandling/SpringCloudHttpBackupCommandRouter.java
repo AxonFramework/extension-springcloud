12c19d290093897915e19b04c6e1debe631b6620
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-01-06 16:28:09 +0100

    Make the HttpBackUp solution fully backwards compatible
    Adjust the SpringCloudHttpBackupCommandRouter to return actual working
    MessageRoutingInformation. As such, it should be the GetMapping again,
    reusing the old request mapping. This should work as the serialized
    format of the MessageRoutingInformation and SerializedMemberCapabilities
     is identical. Lastly, wrap the RestCapabilityDiscoveryMode in an ignore
     instance, as that used to be the behavior.
    
    #23

--- springcloud/src/main/java/org/axonframework/extensions/springcloud/commandhandling/SpringCloudHttpBackupCommandRouter.java
+++ springcloud/src/main/java/org/axonframework/extensions/springcloud/commandhandling/SpringCloudHttpBackupCommandRouter.java
@@ -18,13 +18,17 @@ package org.axonframework.extensions.springcloud.commandhandling;
 
 import org.axonframework.commandhandling.distributed.ConsistentHashChangeListener;
 import org.axonframework.commandhandling.distributed.RoutingStrategy;
-import org.axonframework.commandhandling.distributed.commandfilter.DenyAll;
 import org.axonframework.common.AxonConfigurationException;
+import org.axonframework.extensions.springcloud.commandhandling.mode.IgnoreListingCapabilityDiscoveryMode;
 import org.axonframework.extensions.springcloud.commandhandling.mode.RestCapabilityDiscoveryMode;
+import org.axonframework.extensions.springcloud.commandhandling.mode.SerializedMemberCapabilities;
 import org.axonframework.serialization.Serializer;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.function.Predicate;
@@ -33,12 +37,18 @@ import static org.axonframework.common.BuilderUtils.assertNonEmpty;
 import static org.axonframework.common.BuilderUtils.assertNonNull;
 
 /**
- * Implementation of the {@link SpringCloudCommandRouter} which has a backup mechanism to provide message routing
- * information for other nodes and to retrieve message routing information from other Axon nodes. Used to be a {@link
- * org.springframework.web.bind.annotation.RestController} annotated component, but this has been removed entirely in
- * favor of the new {@link RestCapabilityDiscoveryMode}. As such, this implementation is no more than a wrapper around
- * the {@link SpringCloudCommandRouter} which forces the usages of the {@code RestCapabilityDiscoveryMode}. Due to this,
- * it has been deprecated.
+ * Implementation of the {@link SpringCloudCommandRouter} which has a backup mechanism to provide Message Routing
+ * Information for other nodes and to retrieve Message Routing Information from other Axon nodes.
+ * <p>
+ * It is annotated with the {@link org.springframework.web.bind.annotation.RestController} and contains the {@link
+ * org.springframework.web.bind.annotation.GetMapping} annotated {@link SpringCloudHttpBackupCommandRouter#getLocalMessageRoutingInformation()}
+ * function to have a queryable place to retrieve this node its Message Routing Information. The default endpoint for
+ * this is {@code "/message-routing-information"}. To configure this endpoint the "axon.distributed.spring-cloud.fallback-url"
+ * application property should be adjusted.
+ * <p>
+ * For retrieving the Message Routing Information from other Axon nodes, it uses a {@link
+ * org.springframework.web.client.RestTemplate} to request Message Routing Information through an override of the {@code
+ * SpringCloudHttpBackupCommandRouter#getMessageRoutingInformation(ServiceInstance)} function.
  *
  * @author Steven van Beelen
  * @since 3.1
@@ -46,21 +56,11 @@ import static org.axonframework.common.BuilderUtils.assertNonNull;
  * org.axonframework.extensions.springcloud.commandhandling.mode.RestCapabilityDiscoveryMode}.
  */
 @Deprecated
+@RestController
+@RequestMapping("${axon.distributed.spring-cloud.fallback-url:/message-routing-information}")
 public class SpringCloudHttpBackupCommandRouter extends SpringCloudCommandRouter {
 
-    /**
-     * Instantiate a {@link SpringCloudHttpBackupCommandRouter} based on the fields contained in the {@link Builder}.
-     * <p>
-     * Will assert that the {@link RestTemplate} is not {@code null} and that the {@code
-     * messageRoutingInformationEndpoint} is not {@code null} and empty. This assertion will throw an {@link
-     * AxonConfigurationException} if either of them asserts to {@code true}. All asserts performed by the {@link
-     * SpringCloudCommandRouter.Builder} are also taken into account with identical consequences.
-     *
-     * @param builder the {@link Builder} used to instantiate a {@link SpringCloudHttpBackupCommandRouter} instance
-     */
-    protected SpringCloudHttpBackupCommandRouter(Builder builder) {
-        super(builder);
-    }
+    private final RestCapabilityDiscoveryMode restDiscoveryMode;
 
     /**
      * Instantiate a Builder to be able to create a {@link SpringCloudHttpBackupCommandRouter}.
@@ -78,6 +78,21 @@ public class SpringCloudHttpBackupCommandRouter extends SpringCloudCommandRouter
     }
 
     /**
+     * Instantiate a {@link SpringCloudHttpBackupCommandRouter} based on the fields contained in the {@link Builder}.
+     * <p>
+     * Will assert that the {@link RestTemplate} is not {@code null} and that the {@code
+     * messageRoutingInformationEndpoint} is not {@code null} and empty. This assertion will throw an {@link
+     * AxonConfigurationException} if either of them asserts to {@code true}. All asserts performed by the {@link
+     * SpringCloudCommandRouter.Builder} are also taken into account with identical consequences.
+     *
+     * @param builder the {@link Builder} used to instantiate a {@link SpringCloudHttpBackupCommandRouter} instance
+     */
+    protected SpringCloudHttpBackupCommandRouter(Builder builder) {
+        super(builder);
+        this.restDiscoveryMode = builder.restDiscoveryMode;
+    }
+
+    /**
      * Get the local {@link MessageRoutingInformation}, thus the MessageRoutingInformation of the node this
      * CommandRouter is a part of. Can either be called directly or through a GET operation on the specified {@code
      * messageRoutingInformationEndpoint} of this node.
@@ -86,8 +101,12 @@ public class SpringCloudHttpBackupCommandRouter extends SpringCloudCommandRouter
      * @deprecated in favor of the {@link RestCapabilityDiscoveryMode#getLocalMemberCapabilities()} method
      */
     @Deprecated
+    @GetMapping
     public MessageRoutingInformation getLocalMessageRoutingInformation() {
-        return new MessageRoutingInformation(0, DenyAll.INSTANCE, serializer);
+        SerializedMemberCapabilities serializedCapabilities = restDiscoveryMode.getLocalMemberCapabilities();
+        return new MessageRoutingInformation(serializedCapabilities.getLoadFactor(),
+                                             serializedCapabilities.getSerializedCommandFilter(),
+                                             serializedCapabilities.getSerializedCommandFilterType());
     }
 
     /**
@@ -103,6 +122,7 @@ public class SpringCloudHttpBackupCommandRouter extends SpringCloudCommandRouter
 
         private RestTemplate restTemplate;
         private String messageRoutingInformationEndpoint = "/message-routing-information";
+        private RestCapabilityDiscoveryMode restDiscoveryMode;
 
         @Override
         public Builder discoveryClient(DiscoveryClient discoveryClient) {
@@ -206,13 +226,13 @@ public class SpringCloudHttpBackupCommandRouter extends SpringCloudCommandRouter
         @Override
         protected void validate() {
             // Enforce usage of the RestCapabilityDiscoveryMode by setting it regardless of the provided CapabilityDiscoveryMode
-            capabilityDiscoveryMode(
+            restDiscoveryMode =
                     RestCapabilityDiscoveryMode.builder()
                                                .messageCapabilitiesEndpoint(messageRoutingInformationEndpoint)
                                                .restTemplate(restTemplate)
                                                .serializer(serializerSupplier.get())
-                                               .build()
-            );
+                                               .build();
+            capabilityDiscoveryMode(new IgnoreListingCapabilityDiscoveryMode(restDiscoveryMode));
 
             super.validate();
             assertNonNull(restTemplate, "The RestTemplate is a hard requirement and should be provided");
